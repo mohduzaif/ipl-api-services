bbe5f6d6713df1257d1022839db74c64b857969c
Mohd Uzaif <mohduzaif192@gmail.com>
2025-07-19 02:19:25 +0530

    add one more api(team-record)

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from ipl import API
 
 api_obj = API()
 
-app = Flask("__main__")
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
 
 @app.route('/')
 def index():
     return "Welcome to API Project"
 
+
+
 @app.route('/api/teams')
 def teams():
     response = api_obj.teams_api()
     return response
+
+
+
+@app.route('/api/players')
+def all_unique_players():
+    response = api_obj.all_unique_players_api()
+    return response
+
 
 
 @app.route('/api/teamvsteam')
@@ -22,6 +34,15 @@ def team_vs_team():
 
     response = api_obj.team_vs_team_api(team1, team2)
 
-    return response
+    return jsonify(response)
+
+@app.route('/api/team-record')
+def team_record():
+    team = request.args.get('team')
+
+    response = api_obj.team_record_api(team)
+
+    return jsonify(response)
+
 
 app.run(debug=True)
--- ipl.py
+++ ipl.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from flask import jsonify
+import ast
+
+
 
 # import the dataset from local machine.
 matches = pd.read_csv('./datasets/ipl-matches.csv')
@@ -15,6 +18,24 @@ class API:
         response['Teams'] = list(set(list(matches['Team1']) + list(matches['Team2'])))
         return jsonify(response)
     
+
+    # this function will return the total number of players played IPL match.
+    def all_unique_players_api(self):
+
+        if isinstance(matches['Team1Players'].iloc[0], str):
+            matches['Team1Players'] = matches['Team1Players'].apply(ast.literal_eval)
+            matches['Team2Players'] = matches['Team2Players'].apply(ast.literal_eval)
+            
+        all_players = matches['Team1Players'].sum() + matches['Team2Players'].sum()
+        unique_players = list(set(all_players))
+
+        response = {}
+        response['List of All PLayer'] = unique_players
+        response['IPL Played by Number of Players'] = len(unique_players)
+
+        return jsonify(response) 
+
+
 
     # this function return the track record of the given teams
     def team_vs_team_api(self, team1, team2):
@@ -32,4 +53,34 @@ class API:
         response[team2] = won_by_team2
         response['Draw'] = draw_matches
         
-        return jsonify(response) 
+        return response
+
+
+    def team_record_api(self, team):
+
+        response = {}
+
+        temp_df = matches[(matches['Team1'] == team) | (matches['Team2'] == team)]
+        
+        total_match_played = temp_df.shape[0]
+        matches_won = temp_df[temp_df['WinningTeam'] == team].shape[0] 
+        matches_draw = temp_df[temp_df['WinningTeam'].isnull()].shape[0]
+        matches_loss = total_match_played - (matches_won + matches_draw)
+    
+        inner_dict = {}
+        for t in list(set(list(matches['Team1'].unique()) + list(matches['Team2'].unique()))): 
+            if t != team:
+                result = API().team_vs_team_api(team, t)
+                # print(result)
+                inner_dict[t] = result
+
+        response['All Records'] = {
+            'Total Match Played' : total_match_played, 
+            'Matches Won' : matches_won, 
+            'Matches Loss' : matches_loss, 
+            'Draw' : matches_draw
+        }
+        response['Against'] = inner_dict
+        print(response)
+
+        return response
